c0ea07dff1c758fc109e33030e7f27f2ede2b180
Marc Heinrich <mheinric@ids-hawk>
2019-03-22 19:31:46 +0100

    Adapting to tensorflow 1.4
    Editing all occurrences of keepdims to keep_dims if related to
    tensorflow. Keeping if they are from numpy.

--- baselines/ddpg_cnn/ddpg_cnn_learner.py
+++ baselines/ddpg_cnn/ddpg_cnn_learner.py
@@ -26,13 +26,13 @@ def denormalize(x, stats):
         return x
     return x * stats.std + stats.mean
 
-def reduce_std(x, axis=None, keepdims=False):
-    return tf.sqrt(reduce_var(x, axis=axis, keepdims=keepdims))
+def reduce_std(x, axis=None, keep_dims=False):
+    return tf.sqrt(reduce_var(x, axis=axis, keep_dims=keep_dims))
 
-def reduce_var(x, axis=None, keepdims=False):
-    m = tf.reduce_mean(x, axis=axis, keepdims=True)
+def reduce_var(x, axis=None, keep_dims=False):
+    m = tf.reduce_mean(x, axis=axis, keep_dims=True)
     devs_squared = tf.square(x - m)
-    return tf.reduce_mean(devs_squared, axis=axis, keepdims=keepdims)
+    return tf.reduce_mean(devs_squared, axis=axis, keep_dims=keep_dims)
 
 def get_target_updates(vars, target_vars, tau):
     logger.info('setting up target updates ...')
